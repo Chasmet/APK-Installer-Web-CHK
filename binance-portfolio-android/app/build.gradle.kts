e33e04d714d3a74e903e780b122f71af2253d0cd
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-08-22 07:04:40 +0200

    Add CHK Binance app Gradle config

---
+++ binance-portfolio-android/app/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.chk.binanceportfolio"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.chk.binanceportfolio"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}

bdd667fd30590b1a8ab118a4aea05ec2be43e452
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-08-22 13:59:57 +0200

    Bump CHK Binance Android to v6

--- binance-portfolio-android/app/build.gradle.kts
+++ binance-portfolio-android/app/build.gradle.kts
@@ -7,7 +7,7 @@ android {
         applicationId = "com.chk.binanceportfolio"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "5.0"
+        versionCode = 6
+        versionName = "6.0"
     }
 }
